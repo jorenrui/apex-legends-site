70adb9e0a88f88064ba4c481a17ddf681095ee87
Joeylene Rivera <joeylenerivera@gmail.com>
2021-10-15 01:24:20 +0800

    refactor: relocate character images

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
       <div className="-z-1" aria-hidden="true">
         <div className="h-screen w-screen bg-dirt opacity-50 scale-x-[-1]" />
         <img alt="Apex Legends Logo" src="./assets/img/logo.png" className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-1/2" />
-        <img alt="Bloodhound" src="./assets/img/bloodhound.png" className="absolute -bottom-1/3 left-1/2 -translate-x-1/2 w-[480px]" />
+        <img alt="Bloodhound" src="./assets/img/characters/bloodhound.png" className="absolute -bottom-1/3 left-1/2 -translate-x-1/2 w-[480px]" />
       </div>
     </div>
   )
--- src/components/NextCharacter.tsx
+++ src/components/NextCharacter.tsx
@@ -4,7 +4,7 @@ export function NextCharacter() {
   return (
     <div className="z-10 absolute top-1/2 right-0 -translate-y-1/2 mx-4">
       <div className="relative w-52 h-44 bg-white rounded-blob">
-        <img alt="Wattson" src="./assets/img/wattson.png" className="absolute -top-32 left-6 w-40 h-auto" />
+        <img alt="Wattson" src="./assets/img/characters/wattson.png" className="absolute -top-32 left-6 w-40 h-auto" />
         <div className="absolute left-6 -bottom-32 text-gray-900 text-center">
           <h2 className="uppercase text-3xl font-bold tracking-wide">Wattson</h2>
           <p className="text-base">Static Defender</p>
