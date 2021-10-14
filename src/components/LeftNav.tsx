51d08b7c868b60a8f7ecf1bae7ab1f509c455409
Joeylene Rivera <joeylenerivera@gmail.com>
2021-10-15 01:10:17 +0800

    refactor: replace container with aside in left nav

--- src/components/LeftNav.tsx
+++ src/components/LeftNav.tsx
@@ -1,15 +1,17 @@
 export function LeftNav() {
   return (
-    <ul className="z-10 absolute top-1/2 left-0 -translate-y-1/2 list-none mx-4 w-auto flex flex-col gap-y-2">
-      <li className="py-6 px-3 bg-gray-800 rounded-3xl">
-        <img src="./assets/img/cup.png" className="h-10 w-auto m-auto" />
-      </li>
-      <li className="py-6 px-3 bg-gray-800 rounded-3xl">
-        <img src="./assets/img/gun.png" className="h-10 w-auto m-auto" />
-      </li>
-      <li className="py-6 px-3 bg-gray-800 rounded-3xl">
-        <img src="./assets/img/headshot.png" className="h-10 w-auto m-auto" />
-      </li>
-    </ul>
+    <aside className="z-10 absolute top-1/2 left-0 -translate-y-1/2">
+      <ul className="list-none mx-4 w-auto flex flex-col gap-y-2">
+        <li className="py-6 px-3 bg-gray-800 rounded-3xl">
+          <img src="./assets/img/cup.png" className="h-10 w-auto m-auto" />
+        </li>
+        <li className="py-6 px-3 bg-gray-800 rounded-3xl">
+          <img src="./assets/img/gun.png" className="h-10 w-auto m-auto" />
+        </li>
+        <li className="py-6 px-3 bg-gray-800 rounded-3xl">
+          <img src="./assets/img/headshot.png" className="h-10 w-auto m-auto" />
+        </li>
+      </ul>
+    </aside>
   );
 }
