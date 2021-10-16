02e416c5c3667d2c062a9fe46bfd856352d2d4a6
Joeylene Rivera <joeylenerivera@gmail.com>
2021-10-16 18:09:00 +0800

    feat: add mobile nav

--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -1,10 +1,13 @@
 export function Character() {
   return (
-    <div className="z-10 absolute bottom-12 left-32 bg-white p-8 w-56 h-32 2xl:w-80 2xl:h-44 rounded-blob-2 flex items-center justify-center">
-      <div className="text-gray-900 text-center">
-        <h1 className="text-2xl 2xl:text-4xl font-bold tracking-wide uppercase">Bloodhound</h1>
-        <p className="text-base">Technological Tracker</p>
+    <>
+      <img alt="Bloodhound" src="./assets/img/characters/bloodhound.png" className="z-10 absolute -bottom-1/3 left-1/2 -translate-x-1/2 h-[115%] md:h-[125vh]" />
+      <div className="z-10 absolute bottom-12 left-32 bg-white p-8 w-56 h-32 2xl:w-80 2xl:h-44 rounded-blob-2 flex items-center justify-center">
+        <div className="text-gray-900 text-center">
+          <h1 className="text-2xl 2xl:text-4xl font-bold tracking-wide uppercase">Bloodhound</h1>
+          <p className="text-base">Technological Tracker</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,36 +1,82 @@
-import { SearchIcon } from '@heroicons/react/outline';
+
+import { Disclosure } from '@headlessui/react';
+import { SearchIcon, XIcon, MenuIcon } from '@heroicons/react/outline';
+
+const NAVIGATION = [
+  {
+    text: 'About',
+    link: '/about',
+  },
+  {
+    text: 'Legends',
+    link: '/legends',
+  },
+  {
+    text: 'Seasons',
+    link: '/seasons',
+  },
+  {
+    text: 'Shop',
+    link: '/shop',
+  },
+];
 
 export function Nav() {
   return (
-    <nav className="z-20 p-8 bg-transparent flex items-center justify-between text-base font-medium text-gray-900">
-      <img src="./assets/img/favicon.svg" className="h-14 w-14" />
-      <ul className="list-none flex items-center gap-x-32">
-        <li>
-          <a href="/about" className="border-b-2 border-transparent no-underline hover:border-gray-900">
-            About
-          </a>
-        </li>
-        <li>
-          <a href="/legends" className="border-b-2 border-transparent no-underline hover:border-gray-900">
-            Legends
-          </a>
-        </li>
-      </ul>
-      <ul className="list-none flex items-center gap-x-32">
-        <li>
-          <a href="/seasons" className="border-b-2 border-transparent no-underline hover:border-gray-900">
-            Seasons
-          </a>
-        </li>
-        <li>
-          <a href="/shop" className="border-b-2 border-transparent no-underline hover:border-gray-900">
-            Shop
-          </a>
-        </li>
-      </ul>
-      <div className="flex items-center">
-        <SearchIcon className="h-6 w-6" />
-      </div>
-    </nav>
+    <Disclosure as="nav">
+      {({ open }) => (
+        <>
+          <div className="z-20 px-8 py-2 flex items-center justify-between text-base font-medium text-gray-900 md:p-8">
+            <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-900 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white md:hidden">
+              <span className="sr-only">Open main menu</span>
+              {open ? (
+                <XIcon className="block h-6 w-6" aria-hidden="true" />
+              ) : (
+                <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+              )}
+            </Disclosure.Button>
+            <a href="/">
+              <img src="./assets/img/favicon.svg" className="h-14 w-14" />
+            </a>
+            <ul className="hidden list-none items-center gap-x-28 md:flex">
+              {NAVIGATION.map((item, index) => index < 2 && (
+                <li key={item.text}>
+                  <a href={item.link} className="p-1 border-b-2 border-transparent no-underline hover:text-white hover:border-white focus:text-white focus:border-white">
+                    {item.text}
+                  </a>
+                </li>
+              ))}
+            </ul>
+            <ul className="hidden list-none items-center gap-x-28 md:flex">
+              {NAVIGATION.map((item, index) => index > 1 && (
+                <li key={item.text}>
+                  <a href={item.link} className="p-1 border-b-2 border-transparent no-underline hover:text-white hover:border-white focus:text-white focus:border-white">
+                    {item.text}
+                  </a>
+                </li>
+              ))}
+            </ul>
+            <div className="flex items-center">
+              <SearchIcon className="h-6 w-6 hover:text-white focus:text-white" />
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <ul className="py-2 px-8 space-y-1">
+              {NAVIGATION.map((item) => (
+                <li>
+                  <a
+                    href={item.link}
+                    className="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-900 hover:text-white "
+                  >
+                    {item.text}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
   );
 }
